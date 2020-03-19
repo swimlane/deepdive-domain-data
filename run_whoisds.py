09e8990846c46440e8f24a6c1b44e8ecda7039c6
Josh Rickard <josh.rickard@swimlane.com>
2020-03-18 23:49:26 -0500

    added utc time

--- run_whoisds.py
+++ run_whoisds.py
@@ -6,4 +6,4 @@ from corona import WhoisDs
 save_path = './data/whoisds_files/{}/'.format(datetime.today().strftime('%Y-%m-%d'))
 
 # Currently set to run for previous day (see .add(days=-1) below) to ensure availability
-whoisds = WhoisDs(date=pendulum.now().add(days=-1), save_path=save_path).run()
+whoisds = WhoisDs(date=pendulum.now('UTC').add(days=-1), save_path=save_path).run()
