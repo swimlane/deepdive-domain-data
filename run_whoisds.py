8e38741fad8e42d59fa9d90ff51d401b867315e2
nikkuman <nick.tausek@swimlane.com>
2020-03-18 18:25:44 -0600

    Added WhoisDs Functionality

--- run_whoisds.py
+++ run_whoisds.py
@@ -5,5 +5,5 @@ from corona import WhoisDs
 
 save_path = './data/whoisds_files/{}/'.format(datetime.today().strftime('%Y-%m-%d'))
 
-# Currently set to run for previous day (see days=-1) to ensure availability
+# Currently set to run for previous day (see .add(days=-1) below) to ensure availability
 whoisds = WhoisDs(date=pendulum.now().add(days=-1), save_path=save_path).run()
